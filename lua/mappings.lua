1d8f152a4f68851da6f51704f1650d91020613f3
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-10-31 18:14:28 +0000

    Added custom options and mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,19 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- move lines
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append line from below to current
+map("n", "J", "mzJ`z")
+
+-- keep cursor in the middle
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in the middle
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,20 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+
+o.expandtab = true
+
+o.smartindent = true
+
+o.scrolloff = 10
+
+o.hlsearch = false
+o.incsearch = true
+
+o.termguicolors = true
