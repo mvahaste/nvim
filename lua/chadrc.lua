2a2484783ab19dd77963ee60121f946b2aaa0248
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-01 11:04:10 +0000

    *

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,18 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onedark",
   transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 M.ui = {
@@ -21,11 +21,11 @@ M.ui = {
   },
   tabufline = {
     modules = {
-      btns = function ()
+      btns = function()
         return ""
-      end
-    }
-  }
+      end,
+    },
+  },
 }
 
 return M
