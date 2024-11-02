b478d2d1b5db2ec5cfcbc6e7b129735c972c3ff5
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-02 18:55:12 +0000

    Tab width 2, use spaces

--- lua/options.lua
+++ lua/options.lua
@@ -3,11 +3,10 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
 
--- o.tabstop = 4
--- o.softtabstop = 4
--- o.shiftwidth = 4
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
 
 o.expandtab = true
 
